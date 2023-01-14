28e77575f7e0e7e626c5ec9563908403983fb3c9
unknown <hemant38056@gmail.com>
2023-01-14 22:46:52 +0530

    Submission2

--- src/main/java/com/driver/models/Blog.java
+++ src/main/java/com/driver/models/Blog.java
@@ -34,37 +34,48 @@ public class Blog {
     public int getId() {
         return id;
     }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
 
     public String getTitle() {
         return title;
     }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+
 
 
     public String getContent() {
         return content;
     }
 
-
-    public Date getPubdate() {
-        return pubdate;
-    }
-
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public void setContent(String content) {
         this.content = content;
     }
 
+
+
+    public Date getPubdate() {
+        return pubdate;
+    }
     public void setPubdate(Date pubdate) {
         this.pubdate = pubdate;
     }
+
+
+
+
+
+
+
+
+
 
 
 
