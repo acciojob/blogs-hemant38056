615f1609edb17bcb9673e19518fa5fd1b8f56053
unknown <hemant38056@gmail.com>
2023-01-14 23:12:12 +0530

    Submission4

--- src/main/java/com/driver/models/User.java
+++ src/main/java/com/driver/models/User.java
@@ -26,6 +26,48 @@ public class User {
         this.lastname = lastname;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     public List<Blog> blogList;
 
